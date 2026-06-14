e81f54f99fbb9cbac02e5425c9b37a2a597b1ed7
Eliseo Cohen Imach <12074838+eliseoci@users.noreply.github.com>
2026-06-14 03:23:28 -0300

    test(pipelines): build account-consistent item fixtures

--- spec/models/pipeline_item_spec.rb
+++ spec/models/pipeline_item_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe PipelineItem do
+  let(:account) { create(:account) }
+  let(:pipeline) { create(:pipeline, account: account) }
+  let(:stage) { create(:pipeline_stage, account: account, pipeline: pipeline) }
+  let(:contact) { create(:contact, account: account) }
+
   describe 'associations' do
     it { is_expected.to belong_to(:account) }
     it { is_expected.to belong_to(:pipeline) }
@@ -16,21 +21,21 @@ RSpec.describe PipelineItem do
     it { is_expected.to validate_numericality_of(:value).is_greater_than_or_equal_to(0).allow_nil }
 
     it 'rejects a pipeline from another account' do
-      item = build(:pipeline_item, pipeline: create(:pipeline))
+      item = build_pipeline_item(pipeline: create(:pipeline))
 
       expect(item).not_to be_valid
       expect(item.errors[:pipeline]).to be_present
     end
 
     it 'rejects a stage from another account' do
-      item = build(:pipeline_item, stage: create(:pipeline_stage))
+      item = build_pipeline_item(stage: create(:pipeline_stage))
 
       expect(item).not_to be_valid
       expect(item.errors[:stage]).to be_present
     end
 
     it 'rejects a stage from another pipeline in the same account' do
-      item = build(:pipeline_item)
+      item = build_pipeline_item
       item.stage = create(:pipeline_stage, account: item.account)
 
       expect(item).not_to be_valid
@@ -38,21 +43,21 @@ RSpec.describe PipelineItem do
     end
 
     it 'rejects a contact from another account' do
-      item = build(:pipeline_item, contact: create(:contact))
+      item = build_pipeline_item(contact: create(:contact))
 
       expect(item).not_to be_valid
       expect(item.errors[:contact]).to be_present
     end
 
     it 'rejects an owner from another account' do
-      item = build(:pipeline_item, owner: create(:user))
+      item = build_pipeline_item(owner: create(:user))
 
       expect(item).not_to be_valid
       expect(item.errors[:owner]).to be_present
     end
 
     it 'rejects a team from another account' do
-      item = build(:pipeline_item, team: create(:team))
+      item = build_pipeline_item(team: create(:team))
 
       expect(item).not_to be_valid
       expect(item.errors[:team]).to be_present
@@ -61,9 +66,16 @@ RSpec.describe PipelineItem do
 
   describe '#display_title' do
     it 'uses the contact name when the optional title is blank' do
-      item = build(:pipeline_item, title: nil)
+      item = build_pipeline_item(title: nil)
 
       expect(item.display_title).to eq(item.contact.name)
     end
+  end
+
+  def build_pipeline_item(attributes = {})
+    build(
+      :pipeline_item,
+      { account: account, pipeline: pipeline, stage: stage, contact: contact }.merge(attributes)
+    )
   end
 end
